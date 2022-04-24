ed8982579de005e24ea6396bb309b6d99680b5fe
Semesse <Semperia@outlook.com>
2022-04-25 00:38:39 +0800

    chore: configure jest & misc settings

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+export default {
+  testEnvironment: 'node',
+  testMatch: ['**/*.test.ts'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.ts'],
+  coverageReporters: ['text', 'lcov'],
+  coverageDirectory: 'coverage',
+  coveragePathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/dist/',
+    '<rootDir>/coverage/',
+    '<rootDir>/src/__tests__/',
+    '<rootDir>/src/__mocks__/',
+    '<rootDir>/src/__fixtures__/',
+  ],
+}
