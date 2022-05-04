f769a1086b205615dca2e9c4410585be94c7baf3
Semesse <Semperia@outlook.com>
2022-05-04 22:00:27 +0800

    feat: grant and revoke permits of semaphore

--- src/control/semaphore.test.ts
+++ src/control/semaphore.test.ts
@@ -1,5 +1,5 @@
 import { noExit } from './no_exit'
-import { Semaphore } from './semaphore'
+import { Semaphore, transfer } from './semaphore'
 
 describe('semaphore', () => {
   beforeAll(() => {
@@ -16,19 +16,22 @@ describe('semaphore', () => {
   })
 
   it('should release once', async () => {
-    const sem = new Semaphore(5)
+    const permits = 5
+    const sem = new Semaphore(permits)
+    expect(sem.permits).toBe(permits)
     const release = await sem.acquire()
-    expect(sem.remain).toBe(4)
+    expect(sem.remain).toBe(permits - 1)
     release()
-    expect(sem.remain).toBe(5)
+    expect(sem.remain).toBe(permits)
     release()
-    expect(sem.remain).toBe(5)
+    expect(sem.remain).toBe(permits)
     expect(sem.isFull).toBe(false)
     expect(sem.isEmpty).toBe(true)
   })
 
   it('should be able to acquire', async () => {
-    const sem = new Semaphore(5)
+    const permits = 5
+    const sem = new Semaphore(permits)
     const release = await sem.acquire()
     sem.tryAcquire()
     const release2 = await sem.acquire()
@@ -38,11 +41,12 @@ describe('semaphore', () => {
     await sem.acquire()
     // will not release
     release()
-    expect((async () => sem.tryAcquire())()).rejects.toThrow()
+    expect((async () => sem.tryAcquire())()).rejects.toThrowErrorMatchingInlineSnapshot(`"can't acquire semaphore"`)
 
-    expect(sem.acquire(200)).rejects.toThrow('timeout')
+    expect(sem.acquire(200)).rejects.toThrowErrorMatchingInlineSnapshot(`"timeout"`)
+    expect(sem.acquire(-1)).rejects.toThrowErrorMatchingInlineSnapshot(`"timeout must be valid"`)
     expect(sem.remain).toBe(0)
-    jest.runOnlyPendingTimers()
+    jest.runAllTimers()
     expect(sem.isFull).toBe(true)
     expect(sem.isEmpty).toBe(false)
     expect(sem.remain).toBe(0)
@@ -50,5 +54,57 @@ describe('semaphore', () => {
 
     expect(sem.acquire()).resolves.toBeUndefined()
     setTimeout(release2, 200)
+  })
+
+  it('should be able to grant', async () => {
+    const sem = new Semaphore(1)
+    await sem.acquire()
+    sem.grant(1)
+    expect(sem.permits).toBe(2)
+    expect(() => sem.grant(-1)).toThrowErrorMatchingInlineSnapshot(`"permits must be positive"`)
+
+    return expect(sem.acquire()).resolves.toBeDefined()
+  })
+
+  it('should be able to revoke', async () => {
+    const sem = new Semaphore(1)
+    const release = await sem.acquire()
+    await sem.revoke(1)
+    expect(sem.permits).toBe(0)
+    expect(sem.acquire(0)).rejects.toThrowErrorMatchingInlineSnapshot(`"timeout"`)
+    jest.runAllTimers()
+
+    expect(sem.revoke(1)).rejects.toThrowErrorMatchingInlineSnapshot(`"does not have that much permits"`)
+    expect(sem.revoke(-1)).rejects.toThrowErrorMatchingInlineSnapshot(`"permits must be positive"`)
+  })
+
+  it('grant and revoke infinity should work', async () => {
+    const sem = new Semaphore()
+    const release = await sem.acquire()
+    sem.grant(Infinity)
+    expect(sem.remain).toBe(Infinity)
+    expect(sem.permits).toBe(Infinity)
+    await sem.revoke(Infinity)
+    expect(sem.remain).toBe(0)
+    release()
+    expect(sem.remain).toBe(0)
+  })
+
+  it('transfer permits between semaphores', async () => {
+    const sem1 = new Semaphore(1)
+    const sem2 = new Semaphore(1)
+    const release1 = await sem1.acquire()
+    const release2 = await sem2.acquire()
+    const t = transfer(sem1, sem2, 1)
+    expect(sem1.remain).toBe(0)
+    expect(sem2.remain).toBe(0)
+    expect(sem1.isFull).toBe(true)
+    expect(sem1.permits).toBe(0)
+    expect(sem2.permits).toBe(1)
+    release1()
+    release2()
+    await t
+    expect(sem1.permits).toBe(0)
+    expect(sem2.permits).toBe(2)
   })
 })
--- src/control/semaphore.ts
+++ src/control/semaphore.ts
@@ -13,6 +13,8 @@ import { Future } from './future'
  */
 export class Semaphore {
   #permits: number
+  #debt = 0
+  // the first {permits} items in the queue are running, others are waiting
   #queue: Future<void>[] = []
 
   constructor(permits?: number) {
@@ -23,21 +25,24 @@ export class Semaphore {
    * acquire semaphore
    * @returns a function to release semaphore
    */
-  async acquire(timeout: number = Infinity) {
+  async acquire(timeout?: number) {
     const self = new Future<void>()
-    if (this.isFull) {
-      // istanbul ignore else
-      if (Number.isFinite(timeout)) {
-        console.log('full', timeout)
+    this.#queue.push(self)
+
+    if (this.#queue.length > this.#permits) {
+      if (!Number.isFinite(timeout) || timeout === undefined) {
+        // continue
+      } else if (timeout >= 0) {
         setTimeout(() => {
           self.reject(new Error('timeout'))
           this.#remove(self)
         }, timeout)
+      } else {
+        throw new Error('timeout must be valid')
       }
       await self
     }
 
-    this.#queue.push(self)
     return this.#release(self)
   }
 
@@ -47,7 +52,25 @@ export class Semaphore {
       this.#queue.push(self)
       return this.#release(self)
     }
-    throw new Error()
+    throw new Error("can't acquire semaphore")
+  }
+
+  grant(permits: number = 1) {
+    if (permits < 0) throw new Error('permits must be positive')
+    this.#resolveNext(permits)
+    this.#permits += permits
+  }
+
+  async revoke(permits: number = 1) {
+    if (permits > this.#permits) throw new Error('does not have that much permits')
+    if (permits < 0) throw new Error('permits must be positive')
+    if (!Number.isFinite(permits)) {
+      this.#permits = 0
+      this.#debt += this.#queue.length
+    } else {
+      this.#permits -= permits
+      this.#debt += permits
+    }
   }
 
   get remain() {
@@ -68,11 +91,21 @@ export class Semaphore {
 
   #release(token: Future<void>) {
     // return a function to release semaphore
-    // should not provide `release` on instance because the order of `release` is not guaranteed
+    // should not provide `release` on semaphore instance because the order of `release` is not guaranteed
     return once(() => {
+      if (this.#debt) {
+        this.#debt--
+      } else {
+        this.#resolveNext()
+      }
       this.#remove(token)
-      this.#queue.at(this.permits)?.resolve()
     })
+  }
+
+  #resolveNext(count: number = 1) {
+    for (let i = this.#permits; i < this.#permits + count; i++) {
+      this.#queue.at(i)?.resolve()
+    }
   }
 
   #remove(token: Future<void>) {
@@ -83,4 +116,9 @@ export class Semaphore {
 
     this.#queue.splice(index, 1)
   }
+}
+
+export const transfer = async (from: Semaphore, to: Semaphore, tokens: number) => {
+  await from.revoke(tokens)
+  to.grant(tokens)
 }
