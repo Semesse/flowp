32a3c7a90e19a9b870ff3cf8dcca6997081537ef
Semesse <Semperia@outlook.com>
2022-05-05 00:50:02 +0800

    feat: utility types

---
+++ src/types/index.d.ts
@@ -0,0 +1,10 @@
+export type Equal<X, Y> = (<G>() => G extends X ? 1 : 2) extends <G>() => G extends Y ? 1 : 2 ? true : false
+export type Not<Cond extends boolean> = Cond extends true ? false : true
+export type NotEqual<X, Y> = Not<Equal<X, Y>>
+
+export type Assert<Cond extends boolean> = Cond extends true ? void : never
+export type AssertEqual<X, Y> = Assert<Equal<X, Y>>
+export type AssertNotEqual<X, Y> = Assert<NotEqual<X, Y>>
+
+export type F<TArgs extends any[] | [], TReturn> = (...args: TArgs) => TReturn
+export type Callable = F<any[], any>
