1750d0a783734309ddf7312c323ec495d61bd0cd
Semesse <Semperia@outlook.com>
2022-05-07 03:03:56 +0800

    feat: add entrypoint

---
+++ src/control/index.ts
@@ -0,0 +1,6 @@
+export { Channel } from './channel'
+export { Mutex } from './mutex'
+export { Semaphore } from './semaphore'
+export { Future } from './future'
+export { PipeAdapter } from './pipeable'
+export { lateinit } from './lateinit'
