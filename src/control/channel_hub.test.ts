c5a235410004c227357db1db64294b366f8898dd
Semesse <Semperia@outlook.com>
2022-07-27 02:34:41 +0800

    feat: redesign channel hub

--- src/control/channel_hub.test.ts
+++ src/control/channel_hub.test.ts
@@ -1,13 +1,14 @@
 import { Channel } from './channel'
 import { ChannelHub } from './channel_hub'
+import { to } from './pipeable'
 
 describe('channel hub', () => {
   it('should compose multiple channels into one', async () => {
     const channel1 = new Channel()
     const channel2 = new Channel()
     const channel3 = new Channel()
-    const channelHub = ChannelHub.from(channel1, channel2, channel3)
-    const stream = channelHub.stream()
+    const channelHub = ChannelHub.from([channel1, channel2, channel3])
+    const stream = channelHub.reader().stream()
     await channel1.send('a')
     await channel2.send('b')
     await channel3.send('c')
@@ -17,11 +18,38 @@ describe('channel hub', () => {
     channelHub.close()
   })
 
-  it('unplug a channel that is not plugged should have no effect', () => {
+  it('create writer', async () => {
+    const hub = new ChannelHub()
+    const w = hub.writer()
+    const r = hub.reader()
+    await w.send(0)
+    expect(await r.receive()).toBe(0)
+  })
+
+  it('should not write on closed hub', async () => {
+    const hub = new ChannelHub()
+    const ch = hub.writer()
+    hub.close()
+    expect(ch.send(0)).rejects.toThrow()
+  })
+
+  it('should pipe', async () => {
+    const hub = new ChannelHub()
     const channel1 = new Channel()
     const channel2 = new Channel()
-    const channelHub = ChannelHub.from(channel1)
-    channelHub.unplug(channel2)
-    expect(channelHub.channels.has(channel2)).toBeFalsy()
+    const reader = hub.reader()
+    channel1.pipe(hub)
+    channel2.pipe(hub)
+
+    const fn = jest.fn()
+    reader.pipe(to((v) => fn(v)))
+    await channel1.send('a')
+    await channel2.send('b')
+    expect(fn).toHaveBeenNthCalledWith(1, 'a')
+    expect(fn).toHaveBeenNthCalledWith(2, 'b')
+
+    reader.unpipe()
+    await channel1.send(0)
+    expect(fn).toBeCalledTimes(2)
   })
 })
--- src/control/channel_hub.ts
+++ src/control/channel_hub.ts
@@ -4,55 +4,55 @@ import { Pipe, PipeSource, PipeTarget, read } from './pipeable'
 /**
  * compose multiple channels into one
  */
-export class ChannelHub<T = unknown> implements Pipe<T, T> {
-  public static from<T>(...channels: Channel<T>[]) {
-    return new ChannelHub(...channels)
+export class ChannelHub<T = unknown> implements PipeTarget<T> {
+  /** a helper function, equivalant to ChannelHub.constructor */
+  public static from<T>(writers?: Channel<T>[], readers?: Channel<T>[]) {
+    return new ChannelHub(writers, readers)
   }
 
-  #channels: Set<Channel<T>> = new Set()
-  #channel: Channel<T> = new Channel()
+  private readers: Channel<T>[]
+  private writers: Channel<T>[]
 
-  public constructor(...channels: Channel<T>[]) {
-    channels.forEach((c) => this.plug(c))
-  }
-
-  public [read](value: T, source?: PipeSource<T>) {
-    this.#channel.send(value)
-  }
-  public pipe(target: PipeTarget<T>) {
-    this.#channel.pipe(target)
-  }
-  public unpipe() {
-    this.#channel.unpipe()
+  public constructor(writers?: Channel<T>[], readers?: Channel<T>[]) {
+    this.readers = readers || []
+    this.writers = writers || []
+    this.writers.forEach((w) => w.pipe(this))
   }
 
-  public plug(channel: Channel<T>) {
-    this.#channels.add(channel)
-    channel.pipe(this.#channel)
+  /**
+   * send a valut to the hub, will be received by all readers or pipe target
+   * @param value
+   */
+  public broadcast(value: T) {
+    this.readers.forEach((r) => r.send(value))
   }
 
-  public unplug(channel: Channel<T>) {
-    if (!this.#channels.has(channel)) return
-    this.#channels.delete(channel)
-    channel.unpipe()
+  /**
+   * get a reader channel that can get messages from channel hub
+   * you can only use one of pipe or readers at the same time
+   */
+  public reader() {
+    const ch = new Channel<T>()
+    this.readers.push(ch)
+    return ch
   }
 
-  public fork() {
-    const channel = new Channel<T>()
-    this.plug(channel)
-    return channel
+  /**
+   * get a writer channel that can send messages to channel hub
+   */
+  public writer() {
+    const ch = new Channel<T>()
+    ch.pipe(this)
+    this.writers.push(ch)
+    return ch
   }
 
-  public stream() {
-    return this.#channel.stream()
+  public [read](value: T) {
+    this.broadcast(value)
   }
 
   public close() {
-    this.#channels.forEach((c) => this.unplug(c))
-    this.#channel.close()
-  }
-
-  public get channels() {
-    return this.#channels
+    this.writers.forEach((ch) => ch.close())
+    this.readers.forEach((ch) => ch.close())
   }
 }
