17c442f04b5bc3e0036ccf2feaff0f542597d17b
Semesse <Semperia@outlook.com>
2022-08-18 00:04:01 +0800

    test: fix mutex tests for breaking change

--- src/control/mutex.test.ts
+++ src/control/mutex.test.ts
@@ -4,19 +4,26 @@ describe('mutex', () => {
   it('should be able to acquire', async () => {
     const mutex = new Mutex()
     const release = await mutex.acquire()
-    expect(mutex.isFull).toBe(true)
-    expect(mutex.isEmpty).toBe(false)
+    expect(mutex.canLock).toBe(false)
     release()
-    expect(mutex.isFull).toBe(false)
-    expect(mutex.isEmpty).toBe(true)
+    expect(mutex.canLock).toBe(true)
   })
 
   it('should release once', async () => {
     const mutex = new Mutex()
-    const release = await mutex.acquire()
+    const release = await mutex.lock()
     release()
     release()
-    expect(mutex.isFull).toBe(false)
-    expect(mutex.isEmpty).toBe(true)
+    expect(mutex.canLock).toBe(true)
+  })
+
+  it('try acquire', async () => {
+    const mutex = new Mutex()
+    const release = mutex.tryAcquire()
+    expect(mutex.canLock).toBe(false)
+    release()
+    expect(mutex.canLock).toBe(true)
+    release()
+    expect(mutex.canLock).toBe(true)
   })
 })
