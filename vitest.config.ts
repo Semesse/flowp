b2ea0acd69d951f95cd61fb1941225b81fd9de5e
Semesse <Semperia@outlook.com>
2022-08-26 03:29:04 +0800

    chore: add missing vitest config

---
+++ vitest.config.ts
@@ -0,0 +1,16 @@
+/// <reference types="vitest" />
+
+// Configure Vitest (https://vitest.dev/config/)
+
+// @ts-ignore
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  test: {
+    /* for example, use global to avoid globals imports (describe, test, expect): */
+    globals: true,
+    coverage: {
+      provider: 'istanbul',
+    },
+  },
+})
