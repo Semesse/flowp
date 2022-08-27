d317b037434c6ce7c8238c40dfea8b17ba920aa1
Semesse <Semperia@outlook.com>
2022-08-27 22:32:20 +0800

    ci: fix coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     globals: true,
     coverage: {
       provider: 'istanbul',
-      reporter: ['text', 'json', 'html'],
+      reporter: ['html', 'clover'],
     },
   },
 })
